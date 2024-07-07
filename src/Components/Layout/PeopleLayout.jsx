4425e7e9a3451eef2a1f3d58481931c82f25f181
WebWithRathor <devrajrathor143@gmail.com>
2024-07-07 22:26:00 +0530

    .

--- src/Components/Layout/PeopleLayout.jsx
+++ src/Components/Layout/PeopleLayout.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import FilterTrending from '../partials/FilterTrending';
+import { Link } from 'react-router-dom';
+import Topnav from '../partials/Topnav';
+import InfiniteScroll from '../partials/InfiniteScroll';
+import instance from '../utils/axios';
 
 const PeopleLayout = () => {
+
+    const [media, setMedia] = useState([])
+    const [page, setpage] = useState(1)
+    const [hasMore, sethasMore] = useState(true)
+    const getPerson =async ()=>{
+        try {
+            const { data } = await instance.get(`/person/popular?page=${page}`)
+            if(data.results.length > 0) {
+              setMedia((prev) => [...prev, ...data.results])
+              setpage(page + 1);
+            }else{
+              sethasMore(false);
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+    }
+
+    const refreshMedia = () => {
+        if (media.length === 0) {
+          getPerson()
+        } else {
+          setpage(1)
+          setMedia([])
+          getPerson()
+        }
+      }
+
+
+    useEffect(()=>{
+        refreshMedia()
+    },[])
+
   return (
-    <div>PeopleLayout</div>
+    <div className='h-full w-full'>
+      <div className="flex px-10 w-full items-center pt-5">
+        <Link className='pt-4 flex gap-3 font-semibold text-xl text-zinc-200 items-center' to='/'><i className="ri-arrow-left-line mt-1"></i> Movie</Link>
+        <Topnav />
+      </div>
+      <InfiniteScroll getTrending={getPerson} hasMore={hasMore} media={media} />
+    </div>
   )
 }
 
